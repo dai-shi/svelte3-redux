752b887a146a9059642e4442b24ec1d2adc02480
daishi <daishi@axlight.com>
2019-09-19 23:02:17 +0900

    v0.2.3

--- dist/src/bind.d.ts
+++ dist/src/bind.d.ts
@@ -1,13 +1,14 @@
-import { Action, Store } from 'redux';
+import { Action as BaseAction, Store } from 'redux';
 /**
  * Take Redux store and return a global state for Svelte.
  * @example
  * import { createStore } from 'redux';
  * import { bind } from 'svelte3-redux';
+ *
  * const store = createStore(reducer);
  * export default bind(store);
  */
-export declare const bind: <S, A extends Action<any>>(store: Store<S, A>) => {
-    subscribe: (run: (value: S) => void, invalidate?: ((value?: S | undefined) => void) | undefined) => () => void;
-    dispatch: import("redux").Dispatch<A>;
+export declare const bind: <State, Action extends BaseAction<any>>(store: Store<State, Action>) => {
+    subscribe: (run: (value: State) => void, invalidate?: ((value?: State | undefined) => void) | undefined) => () => void;
+    dispatch: import("redux").Dispatch<Action>;
 };
--- dist/src/bindTracked.d.ts
+++ dist/src/bindTracked.d.ts
@@ -1,14 +1,15 @@
-import { Action, Store } from 'redux';
+import { Action as BaseAction, Store } from 'redux';
 /**
  * Take Redux store and return a global state for Svelte.
  * With state usage tracking.
  * @example
  * import { createStore } from 'redux';
  * import { bindTracked } from 'svelte3-redux';
+ *
  * const store = createStore(reducer);
  * export default () => bindTracked(store);
  */
-export declare const bindTracked: <S, A extends Action<any>>(store: Store<S, A>) => {
+export declare const bindTracked: <State, Action extends BaseAction<any>>(store: Store<State, Action>) => {
     subscribe: (run: (value: any) => void, invalidate?: ((value?: any) => void) | undefined) => () => void;
-    dispatch: import("redux").Dispatch<A>;
+    dispatch: import("redux").Dispatch<Action>;
 };
